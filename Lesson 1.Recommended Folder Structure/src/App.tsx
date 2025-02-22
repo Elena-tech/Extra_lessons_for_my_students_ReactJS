0d6a527f377704356b134f6a701d4bc35738942b
Elena-tech <55022515+Elena-tech@users.noreply.github.com>
2025-02-22 16:30:25 +0000

    Add files via upload

---
+++ Lesson 1.Recommended Folder Structure/src/App.tsx
@@ -0,0 +1,10 @@
+import "./globalstyles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Recommended Folder Structure </h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
